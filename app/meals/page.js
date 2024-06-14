34f9e68f3abad6a44436f12b2639528de8e84924
sammyjay076 <boakyesamuel076@gmail.com>
2024-06-14 23:41:10 +0000

    feature: added loader using Suspense component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,37 @@
+import { Suspense } from "react";
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+  // const meals = await getMeals();
   return (
-    <main>
-      <h1>Meals Main page</h1>
-      <p>Get all your proper meals here</p>
-      <Link href="meals/mac-and-cheese">
-        <p>Macaroni and cheese</p>
-      </Link>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious means, created{" "}
+          <span className={styles.highlight}> by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it your way it is easy and fun
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
