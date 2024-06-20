7d7a81c8a0b1bc5e169e3fe7914c0f4498a6cd55
sammyjay076 <boakyesamuel076@gmail.com>
2024-06-20 17:57:22 +0000

    started work on useActionState

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred on the page</h1>
+      <p>Failed to create favourite meal</p>
+    </main>
+  );
+}
--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,6 +1,8 @@
 "use client";
 import { useFormStatus } from "react-dom";
 
+
+
 export default function MealsFormSubmit() {
   const { pending } = useFormStatus();
 
